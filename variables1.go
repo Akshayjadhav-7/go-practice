bdb191060923a84c37083bd425dba2bf532d2e93
Akshay <akshay>
2022-04-20 14:15:10 +0530

    first commit

---
+++ variables1.go
@@ -0,0 +1,37 @@
+
+package main
+
+import "fmt"
+
+func main(){
+
+var age = 28
+
+var lenght,width int = 20,10
+
+
+name := "Akshay"
+
+age1 := 25
+
+height := 180
+
+count := 100
+
+name2, age2 := "Akshay", 56
+
+fmt.Println("the name is",name2, "age is",age2)
+
+fmt.Println("The count is",count)
+
+fmt.Println("Name is",name,"Age is",age1,"Height is",height)
+
+fmt.Println("lenght and widht is", lenght,width)
+
+fmt.Println("age is",age)
+
+age = 25
+
+fmt.Println("my age is", age)
+
+}
