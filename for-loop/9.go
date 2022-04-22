9e80cf74d95f9a4b570b4e58c11971252c6578c6
Akshay <akshay>
2022-04-22 15:21:56 +0530

    Feat: Problem 9 Solved

---
+++ for-loop/9.go
@@ -0,0 +1,15 @@
+//Print sum of square of first N numbers
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int = 20
+	var sum int = 0
+
+	for i := 1; i <= n; i++ {
+		sum += i * i
+	}
+	fmt.Printf("%d ", sum)
+}
