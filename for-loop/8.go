5dbe2548d5de8f6a456d0316eb347f3aced3aef4
Akshay <akshay>
2022-04-27 13:07:51 +0530

    Feat: Modified the Code

--- for-loop/8.go
+++ for-loop/8.go
@@ -6,13 +6,12 @@ import "fmt"
 func main() {
 
 	var n int = 20
-	cubes(n)
-}
-func cubes(n int) int {
-	cubes := 0
+
 	for i := 1; i <= n; i++ {
-		cubes = i * i * i
-		fmt.Printf("% d", cubes)
+		fmt.Println("Cube of", i, "is:", cube(i))
 	}
-	return cubes
+}
+
+func cube(n int) int {
+	return n * n * n
 }
