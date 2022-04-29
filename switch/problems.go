46e923b6f71571a3d1042c3c5ca4baeaa2ccd0f5
Parikshit Gothwal <parikshit.g@gopherslab.com>
2022-04-29 18:19:39 +0530

    feat: switch cases problems added

---
+++ switch/problems.go
@@ -0,0 +1,8 @@
+1. Write a C program to print day of week name.
+2. Write a C program print total number of days in a month.
+3. Write a C program to check whether an alphabet is vowel or consonant.
+4. Write a C program to find maximum between two numbers.
+5. Write a C program to check whether a number is even or odd.
+6. Write a C program to check whether a number is positive, negative or zero.
+7. Write a C program to find roots of a quadratic equation.
+8. Write a C program to create Simple Calculator.(addition|subtraction|multiplication|division|modulas|square|cube)
