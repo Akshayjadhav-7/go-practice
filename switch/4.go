d8722b5d77b6d63dd8a4f7fb0043d2920a8b1dbf
Akshay <akshay>
2022-05-07 13:21:53 +0530

    Feat: updated the switch cases

--- switch/4.go
+++ switch/4.go
@@ -1,4 +1,4 @@
-// Write a C program to find maximum between two numbers.
+// Write a go program to find maximum between two numbers.
 package main
 
 import "fmt"
@@ -6,7 +6,7 @@ import "fmt"
 func main() {
 	num1, num2 := 9, 6
 
-	switch (num1 > num2) || (num2 > num1) {
+	switch {
 	case num1 > num2:
 		fmt.Print("num1 is greater than num2")
 	case num2 > num1:
