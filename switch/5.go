ca412b89e33e27aaf125132c2e2a56033554f3c6
Akshay <akshay>
2022-05-07 13:22:58 +0530

    Feat:Updated switch cases

--- switch/5.go
+++ switch/5.go
@@ -1,4 +1,4 @@
-// Write a C program to check whether a number is even or odd.
+// Write a go program to check whether a number is even or odd.
 package main
 
 import "fmt"
@@ -6,7 +6,7 @@ import "fmt"
 func main() {
 	num := 7
 
-	switch (num%2 == 0) || (num%2 != 0) {
+	switch {
 	case num%2 == 0:
 		fmt.Print("Given number is Even")
 	case num%2 != 0:
