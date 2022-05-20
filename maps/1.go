b1416281635c4aefcbcfeab9b50710031d86ef8e
Parikshit Gothwal <parikshit.g@gopherslab.com>
2022-05-20 18:23:18 +0530

    feat: maps problems

---
+++ maps/1.go
@@ -0,0 +1,6 @@
+// 1. Write a Go program which takes country as input from user and prints the country's capital as output.
+// Example
+// input = India  output = New Delhi
+// input = Bangladesh output = Dhaka
+// input = Pakistan output = Islamabad
+// input = Srilanka output = Colombo 
---
+++ maps/2.go
@@ -0,0 +1,3 @@
+// 2. Write a Go program that prints the count of occurence of array elements.
+// Example: {1, 2, 3, 2, 3, 4, 5, 4, 6, 6, 7, 7, 7}
+// 1 = 1, 2 = 2, 3 = 2, 4 = 2, 5 = 1, 6 = 2, 7 = 3
